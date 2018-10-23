47aeab5300a3c6d89b7b84228e79f4c46998f876
Sora0Siro <43938296+Sora0Siro@users.noreply.github.com>
2018-10-23 16:47:30 +0300

    *First use of face library

---
+++ app/src/main/java/com/example/ans/trainingwithlists/test_lib.java
@@ -0,0 +1,13 @@
+package com.example.ans.trainingwithlists;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class test_lib extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_lib);
+    }
+}
